ade4ae8efeffdbae327bfcd6d5d30e40e87f90ab
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-02-27 17:30:35 -0500

    feat []: validation post-css file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: [
     require("postcss-nesting"),
-    require("autoprefixer")
+    require("autoprefixer"),
+    require("postcss-import"),
+    require("postcss-at-rules-variables"),
+    require("postcss-simple-vars"),
+    require("postcss-mixins"),
+    require("postcss-functions"),
+    require("postcss-each"),
+    require("postcss-calc"),
+    require("cssnano")({"preset": "default"}),
+    require("postcss-preset-env")({stage: 0}),
+    require("postcss-reporter")({clearMessages: true})
   ],
 };
