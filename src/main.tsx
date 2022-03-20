d9dadc696ebd2d8247731fcf81de64dd0ca29e39
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-03-20 13:10:20 -0500

    feat: reorder css' files and reset inicial page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import { App } from './page/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
