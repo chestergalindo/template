312a463974e395486be49039d88943099eeabedc
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-07-01 11:08:47 -0500

    ✨ feat: documentation to tst

--- src/utils/plus/plus.test.js
+++ src/utils/plus/plus.test.js
@@ -1,5 +1,14 @@
 import { plus } from './plus';
 
 test('sumar 1 + 2 es igual a 3', () => {
-  expect(plus(1, 2)).toBe(3);
+  //arrange
+  const paramsFirst = 1;
+  const paramsSecond = 2;
+
+  //act
+  const result = plus(paramsFirst, paramsSecond);
+  const expected = 3;
+
+  //Assert
+  expect(result).toBe(expected);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import eslintPlugin from 'vite-plugin-eslint';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '@': '/src',
-      '@css': '/src/css',
-      '@hooks': '/src/hooks',
+      '~': path.resolve(__dirname, 'src'),
+      '@css': path.resolve(__dirname, 'src/css/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
     },
   },
-  plugins: [react()],
+  plugins: [tsconfigPaths(), eslintPlugin(), react()],
 });
